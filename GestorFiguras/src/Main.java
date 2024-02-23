701c874d7f75f5508ad8c6e1dd786755a1671aff
Didac <didac.arevalo@estudiantat.upc.edu>
2024-02-23 20:53:01 +0100

    Segundo commit

--- GestorFiguras/src/Main.java
+++ GestorFiguras/src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+public static void main(String[] args) {
+    Figura[] v = new Figura[4];
+
+    v[0] = new Rectangle(5,3);
+    v[1] = new Cercle(5);
+    v[2] = new Quadrat(5);
+    v[3] = new Cercle(120);
+
+    double res = suma(v);
+
+    System.out.println("suma: "+res);
+
+    print(v);
+
+    sort(v);
+
+    print(v);
+
+}
+
+public static double suma(Figura[] fig){
+    double suma = 0;
+    for(Figura area : fig){
+        suma += fig.area();
     }
+    return suma;
+}
+
+public static void sort(Figura[] fig){
+    array.sort(fig);
 }
